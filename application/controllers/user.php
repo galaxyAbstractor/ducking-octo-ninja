d70523721d028551ea6e91c3e02dbf0f996686b6
Viktor Öberg <a11vikob@student.his.se>
2013-04-13 14:59:20 +0200

    Start of login/userregistration

---
+++ application/controllers/user.php
@@ -0,0 +1,62 @@
+<?php
+class User extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('user_model');
+
+	}
+
+	public function index() {
+		if($this->session->userdata('username') != "") {
+			$this->userIsLoggedIn();
+		} else {
+			$this->loginScreen();
+		}
+	}
+
+	private function registerNewUser() {
+		$userdata = array(
+			'username'	=> $this->input->post("username"),
+			'password'	=> $this->input->post("password"),
+			'email'		=> $this->input->post("email"),
+			'city'		=> $this->input->post("city"),
+			'country'	=> $this->input->post("country"),
+			'birthdate'	=> $this->input->post("birthdate")
+		);
+		
+		$request = $this->user_model->adduser($userdata);
+		
+		if($request) {
+			$this->userIsLoggedIn();
+		} else {
+			// Username already taken
+			// Retry
+		}
+	}
+
+	private function loginScreen() {
+		
+	}
+	
+	private function userIsLoggedIn() {
+
+	}
+
+	public function login() {
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+
+		//TODO: Encrypt password
+
+		$result = $this->user_model->doLogin($username, $password);
+		
+		if($result) {
+			$this->userIsLoggedIn();
+		} else {
+			$this->loginScreen();
+		}
+
+	}
+}
+?>
---
+++ application/models/user_model.php
@@ -0,0 +1,30 @@
+<?php
+
+class User_model extends CI_Model {
+	
+	function __construct() {
+		parent::__construct();
+		$this->load->database();
+	}
+	
+	public function doLogin($username, $password) {
+		$query = $this->db->query("SELECT * FROM Users WHERE username = '$username' AND password = '$password'");
+		if ($query->num_rows() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public function addUser($userdata) {
+		$query = $this->db->query("SELECT username FROM Users WHERE username = '$userdata[username]'");
+		if($query->num_rows() > 0) {
+			return false;
+		} else {
+			$this->db->insert('Users', $userdata);
+			return true;
+		}
+	}
+}
+
+?>
