f74903355fd7464d17d3f69aef8ebf3a9f437ebf
Viktor Öberg <a11vikob@student.his.se>
2013-04-14 12:36:38 +0200

    added avatar to listings show

--- application/controllers/don.php
+++ application/controllers/don.php
@@ -3,9 +3,11 @@ class Don extends CI_Controller {
 
 	public function index(){
 		if(!$this->session->userdata('logged_in')){
+			
 			$this->load->view("header");
 			$this->load->view("menu");
 			$this->load->view("index", array('userinfo' => $this->load->view("user/loginpanel","", true)));
+			
 			
 		} else {
 			$this->load->view("header");
